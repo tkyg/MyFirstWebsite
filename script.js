58faa711a6ab4c54c00603aa4796545c35e0cdeb
tkyg <80114433+tkyg@users.noreply.github.com>
2021-10-17 09:06:14 -0400

    add event click

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-const h2 = document.createElement('h2');
-h2.textContent = 
-document.querySelector('body').appendChild(h2);
+document.getElementById("max").onclick = maxVerstappen;
+
+function maxVerstappen() {
+  document.getElementById("max").innerHTML="Max Verstappen is from Netherlands, and he is 24 years old";
+}
+
+document.getElementById("lewis").onclick = lewisHamilton;
+
+function lewisHamilton() {
+  document.getElementById("lewis").innerHTML="Lewis Hamilton is from United Kingdom, and he is 36 years old";
+}
