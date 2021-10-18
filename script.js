9e488fd4d84e8fc8121290c6ab7c7d21397348e8
tkyg <80114433+tkyg@users.noreply.github.com>
2021-10-17 21:26:03 -0400

    team banner modified use js and css

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+function two() {
+    for (i = 0; i < elements.length; i++) {
+      elements[i].style.flex = "25%";
+    }
+  }
+
 document.getElementById("max").onclick = maxVerstappen;
 
 function maxVerstappen() {
@@ -8,4 +14,11 @@ document.getElementById("lewis").onclick = lewisHamilton;
 
 function lewisHamilton() {
   document.getElementById("lewis").innerHTML="Lewis Hamilton is from United Kingdom, and he is 36 years old";
+}
+
+const driverNames = ["Max Verstappen", "Lewis Hamilton", "Valtteri Bottas"];
+function driver(name, team, age) {
+    return name;
+    return team;
+    return age; 
 }
